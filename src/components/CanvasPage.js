2870c6acd1b514486065b81dc822072740503117
Evan Chisholm <elchisholm248@gmail.com>
2020-09-02 11:58:12 -0400

    added an event for when a new user joins a room

--- src/components/CanvasPage.js
+++ src/components/CanvasPage.js
@@ -4,10 +4,9 @@ import { useIoContext } from './IoContext';
 import { AnimatePresence, motion } from 'framer-motion';
 
 export function CanvasPage({ match }) {
-
     const [isMessageShown, setIsMessageShown] = useState(false);
+    const { isNewUser, useChangeId } = useIoContext();
 
-    const { useChangeId } = useIoContext();
     // when the page loads join the correct websocket room based on your url
     useChangeId(match.params.id);
 
@@ -17,20 +16,30 @@ export function CanvasPage({ match }) {
         // we set a flag to true that shows you the link was copied
         setIsMessageShown(true);
         setTimeout(() => {
-            setIsMessageShown(false)
-        }, 2000)
+            setIsMessageShown(false);
+        }, 2000);
     }
 
     return (
         <div className="h-screen overflow-hidden flex flex-col items-center">
             <h1 className="text-lg">Invite code: {match.params.id}</h1>
-            <button className="p-6 pt-3 pb-3 bg-blue-300 m-5 mt-2 mb-2 rounded-lg" onClick={copyLink}>invite friends</button>
-            <Canvas width={750} height={300} className="border-red-500 border-8 canvas" />
+            <button
+                className="p-6 pt-3 pb-3 bg-blue-300 m-5 mt-2 mb-2 rounded-lg"
+                onClick={copyLink}
+            >
+                invite friends
+            </button>
+            <Canvas
+                width={750}
+                height={300}
+                className="border-red-500 border-8 canvas"
+            />
             {/* the animate presence component lets us have unmount and mount animations */}
             <AnimatePresence>
                 {isMessageShown && (
                     // the motion.div component lets us use very fancy animations with framer motion
                     <motion.div
+                        key="copied"
                         initial={{
                             opacity: 0,
                             translateY: -50,
@@ -45,9 +54,22 @@ export function CanvasPage({ match }) {
                         }}
                         className="fixed bottom-0 left-0 m-6 p-3 bg-gray-500 rounded-lg"
                     >
-                        The Link To your canvas has been copied to your clipboard
-                    </motion.div>)}
+                        The Link To your canvas has been copied to your
+                        clipboard
+                    </motion.div>
+                )}
+                {isNewUser && (
+                    <motion.div
+                        key="new user"
+                        initial={{ opacity: 0, translateY: -50 }}
+                        animate={{ opacity: 1, translateY: 0 }}
+                        exit={{ opacity: 0, translateY: 50 }}
+                        className="fixed bottom-0 left-0 m-6 p-3 bg-gray-500 rounded-lg"
+                    >
+                        A new Person has arrived
+                    </motion.div>
+                )}
             </AnimatePresence>
         </div>
-    )
+    );
 }
--- src/components/IoContext.js
+++ src/components/IoContext.js
@@ -1,50 +1,60 @@
-import React, { createContext, useEffect, useContext, useState } from 'react'
+import React, { createContext, useEffect, useContext, useState } from 'react';
 import Io from 'socket.io-client';
 
-const IoContext = createContext()
+const IoContext = createContext();
 
-// const socket = Io('http://localhost:4000')
-const socket = Io('https://virtual-canvas-server.herokuapp.com/')
+// const socket = Io('http://localhost:4000');
+const socket = Io('https://virtual-canvas-server.herokuapp.com/');
 
 // I really don't need the context api for this but I will when I want to implement users
 export function IoProvider({ children }) {
-
     const [roomId, setRoomId] = useState();
+    const [isNewUser, setNewUser] = useState(false);
 
     useEffect(() => {
-        socket.on('current users', users => {
-            console.log(users)
-        })
-    }, [])
+        socket.on('new user', () => {
+            console.log('new user');
+            setNewUser(true);
+            setTimeout(() => {
+                setNewUser(false);
+            }, 1000);
+        });
+    }, []);
 
     function onDraw(e, lastPos) {
-        socket.emit('stroke', { lastPos, currentPos: { x: e.offsetX, y: e.offsetY }, roomId })
+        socket.emit('stroke', {
+            lastPos,
+            currentPos: { x: e.offsetX, y: e.offsetY },
+            roomId,
+        });
     }
 
     // a function that accepts an html canvas Ctx and draws on it whenever there is websocket data
     function onStrokeEvent(ctx) {
-        socket.on('stroke', ({ lastPos, currentPos, }) => {
+        socket.on('stroke', ({ lastPos, currentPos }) => {
             ctx.current.beginPath();
             ctx.current.moveTo(lastPos.x, lastPos.y);
             ctx.current.lineTo(currentPos.x, currentPos.y);
             ctx.current.stroke();
-        })
+        });
     }
 
     // A hook that takes an Id uses the websocket to change to a different room
     function useChangeId(id) {
         useEffect(() => {
-            console.log('changing id')
+            console.log('changing id');
             setRoomId(id);
             socket.emit('change id', id);
-        }, [id])
+        }, [id]);
     }
 
-    return(
-        <IoContext.Provider value={{ onDraw, onStrokeEvent, useChangeId }}>
+    return (
+        <IoContext.Provider
+            value={{ isNewUser, onDraw, onStrokeEvent, useChangeId }}
+        >
             {children}
         </IoContext.Provider>
-    )
+    );
 }
 
 // a hook that lets me only import one function instead of importing useContext and IoContext
